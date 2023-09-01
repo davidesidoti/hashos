701a672f16c879a2b908cedbc9aa66af3e5a84c1
Davide Sidoti <sidotidavide@gmail.com>
2023-09-02 01:57:17 +0200

    main file changes

--- hashos.py
+++ hashos.py
@@ -19,10 +19,40 @@
 
 from lib.hargs import LogLevelEnum
 from lib.hlog import printHeader, hlog
+import json
+import signal
 
 class HashOS:
-	def __init__(self) -> None:
-		printHeader()
+    settings = None
+    cwd = None
+    loopQuit = False
+
+    def __init__(self) -> None:
+        printHeader()
+
+        self.cwd = "/".join(__file__.split("/")[:-1])
+        
+        hlog("Loading settings file...", LogLevelEnum.INFO.value)
+        with open(self.cwd + "/config/settings.json", "r") as settingsFile:
+            self.settings = json.load(settingsFile)
+            settingsFile.close()
+
+        hlog("HashOS Version {}".format(self.settings["general"]["version"]), LogLevelEnum.INFO.value)
+        signal.signal(signal.SIGINT, lambda signal, frame: self.quitLoop())
+        self.loop()
+
+
+    def quitLoop(self) -> None:
+        """
+        The function `quitLoop` sets the `loopQuit` attribute to `True`.
+        """
+        self.loopQuit = True
+
+
+    def loop(self):
+        while not self.loopQuit:
+            pass
+        hlog("Bye! :D", LogLevelEnum.INFO.value)
 
 if __name__ == "__main__":
-	HashOS()
+    HashOS()
